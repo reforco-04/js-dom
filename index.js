efb25ab6a2b8f3efae239e19a03c457da3e57688
reforco-dev <reforcodev.atendimento@gmail.com>
2025-08-26 20:52:37 -0300

    Manipulação do DOM e ouvindo eventos

---
+++ index.js
@@ -0,0 +1,42 @@
+//DOM = Document Object Model = arvore de todos os elementos no html
+
+
+function boasVindas() {
+    const nomeValue = document.querySelector("#nome").value;
+    const conversa = document.querySelector("#conversa");
+    conversa.innerHTML += `<h3>Seja bem vindo(a) ${nomeValue}</h3>`;
+    motivoDoContato();
+}
+
+function motivoDoContato() {
+    const conversa = document.querySelector("#conversa");
+    setTimeout(function () {
+        conversa.innerHTML += `
+            <h3>Qual o motivo do seu contato</h3>
+            <button onclick="queroComprar()">Quero comprar</button>
+            <br>
+            <button onclick="precisoDeSuporte()">Preciso de suporte</button>
+            <br>
+        `
+    }, 1000);
+}
+
+function queroComprar() {
+    const conversa = document.querySelector("#conversa");
+    setTimeout(function () {
+        conversa.innerHTML += '<h3>Confira nosso catálogo: <a href="https://google.com">Link</a></h3>';
+    }, 1000)
+}
+
+function precisoDeSuporte() {
+    const conversa = document.querySelector("#conversa");
+    setTimeout(function () {
+        conversa.innerHTML += `
+            <h3>Preciso de suporte</h3>
+            <button onclick="">Quero resgatar meu produto</button>
+            <br>
+            <button>Falar com atendente</button>
+            <br>
+        `
+    }, 1000);
+}
